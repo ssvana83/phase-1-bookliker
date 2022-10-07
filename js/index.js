f9bc53843393b3d20745f79905c035d735e0b8d1
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2022-10-07 10:30:57 -0400

    commit tree ed21265f09e47a07fbddcbf84e28c40069d7c77f

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
-document.addEventListener("DOMContentLoaded", function() {});
+// X When the page loads, get a list of books from `http://localhost:3000/books` 
+// X display their titles by creating a `li` for each book 
+// X add each `li` to the `ul#list` element.
+
+// When a user clicks the title of a book, display the book's thumbnail, description,
+// and a list of users who have liked the book. 
+// info should be displayed in the `div#show-panel` element.
+
+document.addEventListener("DOMContentLoaded", function()  {
+
+//DOM SELECTORS
+const base_url = ("http://localhost:3000/books")
+
+
+
+//EVENT LISTENERS
+
+
+
+//FETCHERS
+function getAllbooks() {
+  fetch(base_url)
+  .then(response => response.json())
+  .then(response => response.forEach(book => addBooktoDom(book)))
+}
+
+//RENDER FUNCTIONS
+function addBooktoDom(book) {
+  const listUL = document.getElementById("list")
+  const li = document.createElement("li")
+  li.id = book.id
+  li.textContent = book.title
+  li.addEventListener("click", getBook(e, book))
+  listUL.append(li)
+}
+
+
+
+
+
+//EVENT HANDLERS
+
+
+//INITIALIZERS
+getAllbooks()
+});
+
+
+
+
+
